a53882763a2791fbd35f82d5ee8eb8687d47608f
Ken Fazzone <kfazzone@novusinsight.com>
2021-03-07 19:56:29 -0500

    add laravel-vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
-
-	<!-- @vite -->
-	{{-- If development --}}
-	<script type="module" src="http://localhost:3000/@vite/client"></script>
-	<script type="module" src="http://localhost:3000/resources/scripts/main.js"></script>
+	@vite
 </head>
 
 <body>
